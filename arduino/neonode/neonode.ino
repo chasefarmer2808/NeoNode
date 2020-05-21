0d5c3939d44e54d7a3f1ca9782c7013413d0e85f
chasefarmer2808 <chasefarmer2808@ufl.edu>
2020-05-21 09:45:18 -0400

    sends pixel info json

---
+++ arduino/neonode/neonode.ino
@@ -0,0 +1,62 @@
+#include <WiFi.h>
+#include <WebServer.h>
+#include <Adafruit_NeoPixel.h>
+#include <ArduinoJson.h>
+
+#include "./env.h"
+
+// Update these values for your specific Neopixel strip.
+#define NEOPIXEL_PIN 12
+#define NUM_PIXELS 3
+#define RGB_SETTING NEO_GRB
+const char* STRIP_LAYOUT = "strip";
+const bool SUPPORTS_RGBW = false;
+
+WebServer server(80);
+Adafruit_NeoPixel pixels(NUM_PIXELS, NEOPIXEL_PIN, RGB_SETTING + NEO_KHZ800);
+
+void setup() {
+  Serial.begin(115200);
+  WiFi.begin(ssid, password);
+  pixels.begin();
+
+  while (WiFi.status() != WL_CONNECTED) {
+      delay(500);
+      Serial.print(".");
+  }
+
+  printConnectionInfo();
+
+  server.on("/", sendHeartbeat);
+  server.on("/neopixel", sendNeopixelInfo);
+
+  server.begin();
+  Serial.println("HTTP server started");
+}
+
+void loop() {
+  server.handleClient();
+}
+
+void printConnectionInfo() {
+  Serial.println("");
+  Serial.println("WiFi connected.");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void sendHeartbeat() {
+  server.send(200, "text/plain", "Success");
+}
+
+void sendNeopixelInfo() {
+  StaticJsonDocument<JSON_OBJECT_SIZE(3)> doc;
+  doc["num_pixels"] = NUM_PIXELS;
+  doc["strip_layout"] = STRIP_LAYOUT;
+  doc["supports_rgbw"] = SUPPORTS_RGBW;
+  
+  String res;
+  serializeJson(doc, res);
+  
+  server.send(200, "application/json", res);
+}
